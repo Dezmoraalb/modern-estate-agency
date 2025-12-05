5d0b79c5536d15ddc528c6c117c97eecb1bba56f
dezmoraalb <pasha34523452@gmail.com>
2025-12-05 16:59:31 +0200

    Fix base URL for GitHub Pages assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Ваша повна адреса сайту
   site: 'https://dezmoraalb.github.io',
-  base: '/modern-estate-agency',
+  
+  // 2. Назва репозиторію (ОБОВ'ЯЗКОВО зі слешем на початку)
+  base: '/modern-estate-agency/',
+  
   integrations: [tailwind(), react()],
 });
 
